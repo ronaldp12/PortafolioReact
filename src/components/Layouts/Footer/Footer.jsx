c676ca9527c1400054e745eeea90b2897a8adedd
Ronald patiño <patinoguevararonald@gmail.com>
2025-03-12 17:44:56 -0500

    organizacion del footer y estilos de botones

--- src/components/Layouts/Footer/Footer.jsx
+++ src/components/Layouts/Footer/Footer.jsx
@@ -4,23 +4,23 @@ import { ItemLista } from "../../Ui/ItemLista/ItemLista.jsx"
 export const Footer = () => {
   return (
     <div className="footer">
-            <ItemLista contenido={"Facebook"}>
-                 <i class='bx bxl-facebook'></i> 
+            <ItemLista>
+                <i class="bi bi-facebook"></i>
             </ItemLista>
-            <ItemLista contenido={"Telefono"}>
+            <ItemLista>
                 <i class="bi bi-telephone-fill"></i>
             </ItemLista>
 
-            <ItemLista contenido={"Email"}>
+            <ItemLista>
                 <i class="bi bi-envelope-at-fill"></i>
             </ItemLista>
 
-            <ItemLista contenido={"Github"}>
+            <ItemLista>
                 <i class="bi bi-github"></i>
             </ItemLista>
 
-            <ItemLista contenido={"LinkedIn"}>
-                <i class="bi bi-envelope-at-fill"></i>
+            <ItemLista>
+                <i class="bi bi-linkedin"></i>
             </ItemLista>
     </div>
   )
--- src/components/Pages/TechnicalSkills/TechnicalSkills.jsx
+++ src/components/Pages/TechnicalSkills/TechnicalSkills.jsx
@@ -2,11 +2,37 @@ import { ItemLista } from "../../Ui/ItemLista/ItemLista.jsx"
 
 export const TechnicalSkills = () => {
   return (
-    <div>
+
+    <>
+      <div className="container-technical">
         <h3>Frontend</h3>
         <ItemLista contenido={"React"} />
         <ItemLista contenido={"Html"} />
-    </div>
+        <ItemLista contenido={"CSS"} />
+        <ItemLista contenido={"JavaScript"} />
+        <ItemLista contenido={"Tailwind CSS"} />
+      </div>
+
+      <div className="container-technical">
+        <h3>Control de versiones</h3>
+        <ItemLista contenido={"Git"} />
+        <ItemLista contenido={"Github"} />
+      </div>
+
+      <div className="container-technical">
+        <h3>Educacion y Liderazgo </h3>
+        <ItemLista contenido={"Logica de programacion "} />
+        <ItemLista contenido={"Preparacion"} />
+        <ItemLista contenido={"Conduccion de conferencias"} />
+      </div>
+
+      <div className="container-technical">
+        <h3>Desarrollo web </h3>
+        <ItemLista contenido={"Aplicaciones del lado del cliente"} />
+      </div>
+    </>
+    
+    
   )
 }
 
