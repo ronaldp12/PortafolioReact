266a1851d8479c8a0e8119f650accd2c5e3d3d4d
Ronald patiño <patinoguevararonald@gmail.com>
2025-03-12 19:51:46 -0500

    espera antes de redirigir agregado

--- src/components/Pages/TechnicalSkills/TechnicalSkills.jsx
+++ src/components/Pages/TechnicalSkills/TechnicalSkills.jsx
@@ -4,31 +4,35 @@ export const TechnicalSkills = () => {
   return (
 
     <>
-      <div className="container-technical">
-        <h3>Frontend</h3>
-        <ItemLista contenido={"React"} />
-        <ItemLista contenido={"Html"} />
-        <ItemLista contenido={"CSS"} />
-        <ItemLista contenido={"JavaScript"} />
-        <ItemLista contenido={"Tailwind CSS"} />
-      </div>
+      <div className="container-technical-skills">
 
-      <div className="container-technical">
-        <h3>Control de versiones</h3>
-        <ItemLista contenido={"Git"} />
-        <ItemLista contenido={"Github"} />
-      </div>
+        <div className="container-technical">
+          <h3>Frontend</h3>
+          <ItemLista contenido={"React"} />
+          <ItemLista contenido={"Html"} />
+          <ItemLista contenido={"CSS"} />
+          <ItemLista contenido={"JavaScript"} />
+          <ItemLista contenido={"Tailwind CSS"} />
+        </div>
 
-      <div className="container-technical">
-        <h3>Educacion y Liderazgo </h3>
-        <ItemLista contenido={"Logica de programacion "} />
-        <ItemLista contenido={"Preparacion"} />
-        <ItemLista contenido={"Conduccion de conferencias"} />
-      </div>
+        <div className="container-technical">
+          <h3>Control de versiones</h3>
+          <ItemLista contenido={"Git"} />
+          <ItemLista contenido={"Github"} />
+        </div>
 
-      <div className="container-technical">
-        <h3>Desarrollo web </h3>
-        <ItemLista contenido={"Aplicaciones del lado del cliente"} />
+        <div className="container-technical">
+          <h3>Educacion y Liderazgo </h3>
+          <ItemLista contenido={"Logica de programacion "} />
+          <ItemLista contenido={"Preparacion"} />
+          <ItemLista contenido={"Conduccion de conferencias"} />
+        </div>
+
+        <div className="container-technical">
+          <h3>Desarrollo web </h3>
+          <ItemLista contenido={"Aplicaciones del lado del cliente"} />
+        </div>
+        
       </div>
     </>
     
--- src/components/Ui/BtnSection/BtnSection.jsx
+++ src/components/Ui/BtnSection/BtnSection.jsx
@@ -1,10 +1,20 @@
 import { NavLink } from "react-router-dom"
 
 export const BtnSection = ({contenido, estilos, to}) => {
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    setTimeout(() => {
+
+      window.location.href = to;
+    }, 1000);
+  };
+
   return (
     <>
         <NavLink to={to}>
-          <button className={estilos}>{contenido}</button>
+          <button onClick={handleClick} className={estilos}>{contenido}</button>
         </NavLink>
         
     </>
